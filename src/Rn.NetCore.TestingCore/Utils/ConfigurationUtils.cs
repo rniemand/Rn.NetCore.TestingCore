c2d689cdbf4007718493c8a0bd450083e7b4017f
Richard Niemand <niemand.richard@gmail.com>
2021-07-23 17:25:48 -0600

    Extracting config from objects

---
+++ src/Rn.NetCore.TestingCore/Utils/ConfigurationUtils.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Rn.NetCore.Common.Helpers;
+
+namespace Rn.NetCore.TestingCore.Utils
+{
+  public class ConfigurationUtils
+  {
+    public static IConfiguration FromObject(object config, string key = null)
+    {
+      // TODO: [TESTS] (ConfigurationUtils.FromObject) Add tests
+      var jsonHelper = new JsonHelper();
+      var safeKey = string.IsNullOrWhiteSpace(key) ? string.Empty : key;
+
+      return new ConfigurationBuilder()
+        .AddInMemoryCollection(
+          new ObjectToDictionary(jsonHelper).Extract(config, safeKey)
+        )
+        .Build();
+    }
+  }
+}
