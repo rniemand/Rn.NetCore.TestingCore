bbf1080d11086a764c7a509216735c2a0ba90e9d
Richard Niemand <niemand.richard@gmail.com>
2021-08-04 16:40:39 -0600

    Updating Metric references

--- src/DevConsole/Program.cs
+++ src/DevConsole/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using NLog.Extensions.Logging;
 using Rn.NetCore.Common.Logging;
-using Rn.NetCore.TestingCore.Builders;
 using Rn.NetCore.TestingCore.Utils;
 
 namespace DevConsole
