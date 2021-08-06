08588b27f14c6f64b865c044a5d8877138787b6c
Richard Niemand <niemand.richard@gmail.com>
2021-08-05 18:06:10 -0600

    Added SubmitMetricAsyncValidator

---
+++ src/Rn.NetCore.TestingCore/Metrics/SubmitMetricAsyncValidator.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using NSubstitute;
+using Rn.NetCore.Common.Metrics.Interfaces;
+
+namespace Rn.NetCore.TestingCore.Metrics
+{
+  public class SubmitMetricAsyncValidator
+  {
+    private readonly Dictionary<string, string> _tags;
+
+    public SubmitMetricAsyncValidator()
+    {
+      _tags = new Dictionary<string, string>();
+    }
+
+    public SubmitMetricAsyncValidator ContainsTag(string tag, string expected)
+    {
+      _tags[tag] = expected;
+      return this;
+    }
+
+    private bool PassesValidation(IMetricBuilder builder)
+    {
+      var metric = builder.GetRawMetric();
+
+      foreach (var (key, value) in _tags)
+      {
+        if (!metric.Tags.ContainsKey(key))
+          return false;
+
+        if (metric.Tags[key] != value)
+          return false;
+      }
+
+      return true;
+    }
+
+    public void Run(IMetricService metricService)
+    {
+      metricService.Received(1).SubmitMetricAsync(
+        Arg.Is<IMetricBuilder>(b => PassesValidation(b))
+      );
+    }
+  }
+}
